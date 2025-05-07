a56c0d200b24bbe4a42344a2d97dca0b2011d79d
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2025-05-07 16:09:00 +0100

    feat: add function to calc pension lump sum

---
+++ app/lib/calculateTotalPensionTarget.ts
@@ -0,0 +1,20 @@
+import { INTEREST_RATE } from "./constants";
+
+/**
+ * Lump sum desired at retirement
+ *
+ * @param desiredIncomePerYear - Desired income per year in retirement
+ * @param annualRate - Annual interest rate (default is INTEREST_RATE)
+ * @returns Lump sum required at retirement
+ * @throws Error if annualRate is less than or equal to 0
+ */
+export function calculateTotalPensionTarget(
+  desiredIncomePerYear: number,
+  annualRate: number = INTEREST_RATE
+): number {
+  if (annualRate <= 0) {
+    throw Error("Rate must be greater than 0");
+  }
+
+  return parseFloat((desiredIncomePerYear / annualRate).toFixed(2));
+}
