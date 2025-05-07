10c085324c958de5dfec8a0a2052fe0e515bd3c9
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2025-05-07 16:35:50 +0100

    feat: add max retirement age const

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,4 +1,5 @@
 export const USER_AGE: number = 25;
-export const MIN_RETIREMENT_AGE: number = USER_AGE + 1;
 export const LIFE_EXPECTANCY: number = 81;
 export const INTEREST_RATE: number = 0.049;
+export const MAX_RETIREMENT_AGE: number = LIFE_EXPECTANCY - 1;
+export const MIN_RETIREMENT_AGE: number = USER_AGE + 1;
