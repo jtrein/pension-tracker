61680f88f637270d39abc6f2de01c315fa795f73
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2025-05-07 17:57:45 +0100

    feat: add current pension future value chart

---
+++ app/components/CurrentPotsFutureValueChart.tsx
@@ -0,0 +1,43 @@
+import { ResponsiveContainer, PieChart, Pie, Cell, Legend } from "recharts";
+
+type CurrentPotsFutureValueChartProps = {
+  data: { name: string; value: number }[];
+};
+
+// Pick random colors for the pie chart segments
+const getRandomColor = () => {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+};
+
+export default function CurrentPotsFutureValueChart({
+  data,
+}: Readonly<CurrentPotsFutureValueChartProps>) {
+  return (
+    <ResponsiveContainer width="100%" height={200}>
+      <PieChart>
+        <Pie
+          data={data}
+          dataKey="value"
+          nameKey="name"
+          cx="50%"
+          cy="50%"
+          innerRadius={50}
+          outerRadius={80}
+          label
+        >
+          {data.map((_, i) => (
+            <Cell key={i} fill={getRandomColor()} />
+          ))}
+        </Pie>
+        <Legend verticalAlign="bottom" height={24} />
+      </PieChart>
+    </ResponsiveContainer>
+  );
+}
